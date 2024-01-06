e6aa532a052fa39f019672b4db04205196b39e04
Pythonesa <pythonesamar@gmail.com>
2024-01-06 02:33:20 -0300

    :sparkles: validation on controlled form

--- src/components/Controlado.jsx
+++ src/components/Controlado.jsx
@@ -4,31 +4,41 @@ export default function Controlado() {
     const [todo, setTodo] = useState({
         todoName: '',
         todoDescription: '',
-        todoState: 'Pendiente'
+        todoState: 'Pendiente',
+        todoPriority: true
     })
+
+    const [error, setError] = useState(false)
+
+    const {todoName, todoDescription, todoState, todoPriority} = todo
+    const ShowError = () => <div className="alert alert-danger">Todos los campos son obligatorios</div>
 
     const handleSubmit = (event) => {
         event.preventDefault();
-
+        if (!todoName.trim() || !todoDescription.trim() || !todoState.trim()) return setError(true)
+        setError(false)
     }
 
-    const handleOnChange = (event) => {
+
+    function handleOnChange(event) {
+        const {name, value, type, checked} = event.target
         setTodo({
             ...todo,
-            [event.target.name]: event.target.value
+            [name]: type === 'checkbox' ? checked : value
         })
     }
 
     return (
         <div>
             <h2>Controlado</h2>
+            {error && <ShowError />}
             <form onSubmit={handleSubmit}>
                 <input 
                     type="text" 
                     placeholder="Ingrese el nombre de la tarea" 
                     className="form-control mb-2 " 
                     name="todoName"
-                    value={todo.todoName}
+                    value={todoName}
                     onChange={handleOnChange}
                 />
                 <textarea 
@@ -36,13 +46,24 @@ export default function Controlado() {
                     placeholder="Ingrese la descripción de la tarea" 
                     className="form-control mb-2" 
                     name="todoDescription"
-                    value={todo.todoDescription}
+                    value={todoDescription}
                     onChange={handleOnChange}
                 />
+                <div className="form-check mb-2">
+                    <input
+                        type='checkbox'
+                        name="todoPriority"
+                        id="todoPriority"
+                        className="form-check-input"
+                        onChange={handleOnChange}
+                        checked={todoPriority}
+                    />
+                    <label htmlFor="todoPriority">Prioritario</label>
+                </div>
                 <select 
                     name="todoState" 
                     className="form-select mb-2" 
-                    value={todo.todoState}
+                    value={todoState}
                     onChange={handleOnChange}
                 >
                     <option value="Pendiente">Pendiente</option>
